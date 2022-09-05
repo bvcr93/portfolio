c9a1053ccd4ad854c7f67f3cd94d86227e8ca91e
Darel <darelbavcar1@gmail.com>
2022-09-05 20:05:04 +0200

    Portfolio (React)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Intro from "./components/intro/Intro";
+import About from "./components/intro/About";
+import ProductList from "./components/ProductList";
+import Contact from "./components/Contact";
+import Toggle from "./components/Toggle";
+import { useContext } from "react";
+import { ThemeContext } from "./context";
+
+
 
 function App() {
+ 
+const theme = useContext(ThemeContext)
+const darkMode = theme.state.darkMode
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: darkMode ? "#222" : "white",
+    color: darkMode && "white"}}>
+      <Toggle ></Toggle>
+<Intro ></Intro>
+
+<About ></About>
+<ProductList></ProductList>
+<Contact></Contact>
     </div>
   );
 }
